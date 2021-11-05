4ff7ad85873dd4a7dc775ce0db410ac8ce006cae
puroktaa <fliphd23@gmail.com>
2021-11-05 09:37:05 +0700

    bottz

---
+++ message/help.js
@@ -0,0 +1,255 @@
+exports.gameMenu = (prefix) => {
+    return `
+ 🕹️ GAME-MENU 🕹️
+ 
+*☯➳️* ${prefix}slot
+*☯➳️* ${prefix}limitgame
+*☯➳️* ${prefix}gelud @tag
+*☯➳️* ${prefix}tictactoe @tag
+*☯➳️* ${prefix}siapaaku
+*☯➳️* ${prefix}family100
+*☯➳️* ${prefix}kuismath
+*☯➳️* ${prefix}asahotak
+*☯➳️* ${prefix}tebaklirik
+*☯➳️* ${prefix}tebaklagu
+*☯➳️* ${prefix}tebakkata
+*☯➳️* ${prefix}susunkata
+*☯➳️* ${prefix}kimiakuis
+*☯➳️* ${prefix}caklontong
+*☯➳️* ${prefix}tebakjenaka
+*☯➳️* ${prefix}tebakanime
+*☯➳️* ${prefix}tebaktebakan
+*☯➳️* ${prefix}tebakgambar
+*☯➳️* ${prefix}tebakbendera
+*☯➳️* ${prefix}suit *batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+   # FUN-MENU
+
+*☯➳️* ${prefix}mining
+*☯➳️* ${prefix}togel
+*☯➳️* ${prefix}cekwatak
+*☯➳️* ${prefix}cekmati [nama]
+*☯➳️* ${prefix}wangy [nama]
+*☯➳️* ${prefix}citacita
+*☯➳️* ${prefix}toxic
+*☯➳️* ${prefix}truth
+*☯➳️* ${prefix}dare
+*☯➳️* ${prefix}apakah
+*☯➳️* ${prefix}bisakah
+*☯➳️* ${prefix}kapankah
+*☯➳️* ${prefix}rate
+*☯➳️* ${prefix}jadian
+*☯➳️* ${prefix}cantik
+*☯➳️* ${prefix}ganteng
+*☯➳️* ${prefix}beban
+*☯➳️* ${prefix}babi
+*☯➳️* ${prefix}cekganteng
+*☯➳️* ${prefix}cekcantik
+
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+  #  OTHER-MENU 
+
+*☯➳️* ${prefix}brainly *query*
+*☯➳️* ${prefix}shopee *product*
+*☯➳️* ${prefix}playstore *query*
+*☯➳️* ${prefix}ssweb *query*
+*☯➳️* ${prefix}google *query*
+*☯➳️* ${prefix}image *query*
+*☯➳️* ${prefix}pinterest *query*
+*☯➳️* ${prefix}nulis *teks*
+*☯➳️* ${prefix}iguser *ussername*
+*☯➳️* ${prefix}igstalk *username*
+*☯➳️* ${prefix}githubstalk *username*
+*☯➳️* ${prefix}tiktokstalk *ussername*
+*☯➳️* ${prefix}img2url *reply foto*
+*☯➳️* ${prefix}ytsearch *query*
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+  # STICKER-MENU
+
+*☯➳️* ${prefix}dadu
+*☯➳️* ${prefix}doge
+*☯➳️* ${prefix}toimg
+*☯➳️* ${prefix}patrick
+*☯➳️* ${prefix}garwgura
+*☯➳️* ${prefix}ttg *teks*
+*☯➳️* ${prefix}attp *teks*
+*☯➳️* ${prefix}stickeranime
+*☯➳️* ${prefix}semoji *emoji*
+*☯➳️* ${prefix}sticker *reply foto/video*
+*☯➳️* ${prefix}smeme *teks|teks*
+*☯➳️* ${prefix}swm *pack|author*
+*☯➳️* ${prefix}take *pack|author* 
+*☯➳️* ${prefix}tovideo *reply sgif*
+
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+  # WIBU-MENU
+
+*☯➳️* ${prefix}loli
+*☯➳️* ${prefix}manga
+*☯➳️* ${prefix}anime 
+*☯➳️* ${prefix}lolivideo
+*☯➳️* ${prefix}husbu
+*☯➳️* ${prefix}waifu
+*☯➳️* ${prefix}milf
+*☯➳️* ${prefix}neko
+*☯➳️* ${prefix}kanna
+*☯➳️* ${prefix}sagiri
+*☯➳️* ${prefix}hentai
+*☯➳️* ${prefix}cosplay
+*☯➳️* ${prefix}wallnime
+*☯➳️* ${prefix}kusonime
+*☯➳️* ${prefix}megumin
+*☯➳️* ${prefix}otakudesu
+*☯➳️* ${prefix}doujindesu
+*☯➳️* ${prefix}storyanime
+*☯➳️* ${prefix}nakanomiku
+*☯➳️* ${prefix}nakanonino
+*☯➳️* ${prefix}nakanoitsuki
+*☯➳️* ${prefix}otakuongoing
+*☯➳️* ${prefix}nhentai *code*
+*☯➳️* ${prefix}nekopoi *link*
+*☯➳️* ${prefix}nekopoi3d
+*☯➳️* ${prefix}nekopoicosplay
+*☯➳️* ${prefix}nekopoisearch
+
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+  # OWNER-MENU
+
+*☯➳️* ${prefix}bc *teks*
+*☯➳️* ${prefix}term
+*☯➳️* ${prefix}eval
+*☯➳️* ${prefix}reset
+*☯➳️* ${prefix}clearall
+*☯➳️* ${prefix}leaveall
+*☯➳️* ${prefix}join *link gc*
+*☯➳️* ${prefix}shutdown
+*☯➳️* ${prefix}getquoted
+*☯➳️* ${prefix}addupdate *fiturnya*
+*☯➳️* ${prefix}exif *nama|author*
+*☯➳️* ${prefix}sewa add/del *waktunya*
+*☯➳️* ${prefix}premium add @tag|nomor
+*☯➳️* ${prefix}premium del @tag|nomor
+
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+  # GROUP-MENU
+
+*☯➳️* ${prefix}groupsetting
+*☯➳️* ${prefix}afk *alasan*
+*☯➳️* ${prefix}ceksewa
+*☯➳️* ${prefix}kickall
+*☯➳️* ${prefix}infogrup
+*☯➳️* ${prefix}promote
+*☯➳️* ${prefix}demote
+*☯➳️* ${prefix}listonline
+*☯➳️* ${prefix}tagall *teks*
+*☯➳️* ${prefix}leave
+*☯➳️* ${prefix}kick *reply*
+*☯➳️* ${prefix}add *+62xxxxxx*
+*☯➳️* ${prefix}setgrupname
+*☯➳️* ${prefix}setppgrup
+*☯➳️* ${prefix}setdesc
+*☯➳️* ${prefix}sider *reply chat bot*
+*☯➳️* ${prefix}hidetag *teks/reply teks*
+,
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+  ⬇️DOWNLOAD-MENU⬇️
+
+*☯➳️* ${prefix}fb 
+*☯➳️* ${prefix}igdl 
+*☯➳️* ${prefix}igdl2 
+*☯➳️* ${prefix}twitter 
+*☯➳️* ${prefix}tiktok 
+*☯➳️* ${prefix}play 
+*☯➳️* ${prefix}ythd 
+*☯➳️* ${prefix}ytmp3 
+*☯➳️* ${prefix}ytmp4 
+*☯➳️* ${prefix}soundcloud 
+*☯➳️* ${prefix}tiktoknowm 
+*☯➳️* ${prefix}tiktokaudio
+*☯➳️* ${prefix}mediafire 
+*☯➳️* ${prefix}nhentaipdf *code*
+
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+
+bot ini merupakan program open-source yang ditulis menggunakan Javascript.
+
+Dengan menggunakan bot ini maka anda setuju dengan Syarat dan Kondisi sebagai berikut:
+- bot tidak menyimpan data anda di server kami.
+- - bot tidak *bertanggung jawab* atas sticker yang anda buat dari bot ini serta video, gambar,text maupun data lainnya yang anda dapatkan dari bot ini.
+- bot *tidak boleh* digunakan untuk layanan yang bertujuan/berkontribusi dalam: 
+-     • seks / perdagangan manusia
+    • perjudian
+    •     • perilaku adiktif yang merugikan 1
+    • kejahatan
+    •     • kekerasan (kecuali jika diperlukan untuk melindungi keselamatan publik)
+    • pembakaran hutan / penggundulan hutan
+    •     • *ujaran kebencian atau diskriminasi berdasarkan usia, jenis kelamin, identitas gender, ras, seksualitas, agama, kebangsaan*
+
+- Buat Yang Sudah Sewa Bot:
+ • Jadikan Bot Sebagai Admin
+ • Jadikan Salah Satu Owner Sebagai Admin 
+ • Jika Salah Satu Owner Tidak Dijadikan Sebagai Admin Nanti Kalau Bot Kick Owner GC Nanti Tidak Bisa Di Tambahkan Lagi Nanti GC akan Jadi usang/ Tidak bisa dikembalikan lagi
+
+- Adapun larangan bot itu sendiri sebagai berikut:
+-     • Dilarang melakukan Spamming pada bot dengan maksud apapun
+    • Dilarang melakukan video call pada bot dengan maksud apapun
+    •     • Dilarang melakukan Telp pada bot dengan maksud apapun
+    • Dilarang untuk melakukan abuse Command bot dengan maksud apapun
+    • Melanggar larangan di atas? anda akan terkena _Auto Block_ 
+    • Req UnBlock? bisa Chat wa *Creator*
+Best regard, *Helga Zex*.
+
+*𝕊ℙ𝔼𝕊𝕀𝔸𝕃 𝕋ℍ𝔸ℕ𝕂𝕊 𝔽𝕆ℝ*
+--All users Helga Zex's
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+  # INFO-MENU
+
+*☯➳️* ${prefix}update
+*☯➳️* ${prefix}level
+*☯➳️* ${prefix}rules
+*☯➳️* ${prefix}profile
+*☯➳️* ${prefix}waktu
+*☯➳️* ${prefix}botstat
+*☯➳️* ${prefix}sewabot
+*☯➳️* ${prefix}listsewa
+*☯➳️* ${prefix}owner
+*☯➳️* ${prefix}ping
+*☯➳️* ${prefix}runtime
+*☯➳️* ${prefix}donasi
+*☯➳️* ${prefix}leaderboard
+*☯➳️* ${prefix}cekpremium
+*☯➳️* ${prefix}listpremium
+*☯➳️* ${prefix}sourcecode
+*☯➳️* ${prefix}bugreport *keluhan*
+
+`
+} 
